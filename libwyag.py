6257f45d894144d9536eff3e5eca9c32051755d3
andreamoretti890 <andrea.moretti890@gmail.com>
2024-04-13 10:45:22 +0200

    repository and utils

---
+++ libwyag.py
@@ -0,0 +1,155 @@
+import argparse
+import collections
+import configparser
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
+
+argparser = argparse.ArgumentParser(description="Version control")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+
+# From the first one, so we skip the "wyag" command
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add":
+            cmd_add(args)
+        case "cat-file":
+            cmd_cat_file(args)
+        case "check-ignore":
+            cmd_check_ignore(args)
+        case "checkout":
+            cmd_checkout(args)
+        case "commit":
+            cmd_commit(args)
+        case "hash-object":
+            cmd_hash_object(args)
+        case "init":
+            cmd_init(args)
+        case "log":
+            cmd_log(args)
+        case "ls-files":
+            cmd_ls_files(args)
+        case "ls-tree":
+            cmd_ls_tree(args)
+        case "rev-parse":
+            cmd_rev_parse(args)
+        case "rm":
+            cmd_rm(args)
+        case "show-ref":
+            cmd_show_ref(args)
+        case "status":
+            cmd_status(args)
+        case "tag":
+            cmd_tag(args)
+        case _:
+            print("Bad command.")
+
+
+class GitRepository(object):
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False) -> None:
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("This is not a Git repository %s" % path)
+
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        config = repo_file(self, "config")
+
+        if config and os.path.exists(config):
+            self.conf.read(config)
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            version = int(self.conf.get("core", "repositoryformatversion"))
+            if version != 0:
+                raise Exception("Unsupported repositoryformatversion %s" % version)
+
+
+def repo_path(repo, *path):
+    return os.path.join(repo.gitdir, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    path = repo_path(repo, path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+
+    return None
+
+
+def repo_file(repo, *path, mkdir=False):
+    # Create dir if absent
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+
+def create_repo(path) -> GitRepository:
+    repo = GitRepository(path, force=True)
+
+    # Make sure the path either doesn't exist or is an empty dir.
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("%s is not a directory!" % path)
+        if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
+            raise Exception("%s is not empty!" % path)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write(
+            "Unnamed repository; edit this file 'description' to name the repository.\n"
+        )
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+
+def repo_default_config() -> configparser.ConfigParser:
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    # the version of the gitdir format. 0 means the initial format, 1 the same with extensions
+    ret.set("core", "repositoryformatversion", "0")
+    # disable tracking of file mode (permissions) changes in the work tree
+    ret.set("core", "filemode", "false")
+    # indicates that this repository has a worktree
+    ret.set("core", "bare", "false")
+
+    return ret
